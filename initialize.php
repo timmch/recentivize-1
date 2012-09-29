6160906a4f194590f40a4d62e901511ca400b27e
Nick Carson <carson@textrooms.com>
2012-09-29 09:45:31 -0400

    init

--- initialize.php
+++ initialize.php
@@ -6,7 +6,7 @@
 		case 'CARSON':
 			defined('DS') ? null 		: define('DS',			DIRECTORY_SEPARATOR);
 			
-			defined('SITE_ROOT') ? null : define('SITE_ROOT',	DS.'Library'.DS.'WebServer'.DS.'Documents'.DS.'Spestle'.DS.'Live'.DS);
+//			defined('SITE_ROOT') ? null : define('SITE_ROOT',	DS.'Library'.DS.'WebServer'.DS.'Documents'.DS.'Spestle'.DS.'Live'.DS);
 			defined('WEB_URL') ? null 	: define('WEB_URL',		'http://localhost/recentivize'.DS);
 			break;
 	}

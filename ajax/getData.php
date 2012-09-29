139f62b0b92fe79e3f45c7795687efbaff20c0b9
timmch <timmch@gmail.com>
2012-09-29 09:21:32 -0400

    D

--- ajax/getData.php
+++ ajax/getData.php
@@ -3,15 +3,9 @@
 	$username = "timmch_root";
 	$password = "recentivize";
 	$hostname = "108.167.179.192";
-	$dbhandle = mysql_connect($hostname, $username, $password) 
-	 or die("Unable to connect to MySQL");
-	echo "Connected to MySQL<br>";
-
-	$selected = mysql_select_db("timmch_recentivize",$dbhandle) 
-	  or die("Could not select examples");
-	  
+	$dbhandle = mysql_connect($hostname, $username, $password);
+	$selected = mysql_select_db("timmch_recentivize",$dbhandle);
 	$data = mysql_query("SELECT * FROM users");
-			
 	$output['data'] = $data;
 	$output['err'] = FALSE;
 	$output['msg'] = "Got data: $data";

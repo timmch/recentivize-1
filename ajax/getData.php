e53af02a9c66af7dbaa3795d6c12ced2b9318bd5
timmch <timmch@gmail.com>
2012-09-29 11:03:12 -0400

    D

--- ajax/getData.php
+++ ajax/getData.php
@@ -2,17 +2,26 @@
 	$username = "timmch_root";
 	$password = "recentivize";
 	$hostname = "108.167.179.192"; 
-
 	$dbhandle = mysql_connect($hostname, $username, $password) 
 	  or die("Unable to connect to MySQL");
-	//echo "Connected to MySQL<br>";
-
 
 	$selected = mysql_select_db("timmch_recentivize",$dbhandle);
 	$data = mysql_query("SELECT * FROM users");
-	$output['data'] = $data;
+
+	$returnable;
+	while ($row = mysql_fetch_row($data)) {
+		$rowt['id'] = $row[0];
+		$rowt['first_name'] = $row[1];
+		$rowt['last_name'] = $row[2];
+		array_push($returnable, $rowt);
+	}
+	echo($returnable[0]['first_name']);
+	echo("//");
+
+	//echo($users_name);
+	$output['data'] = $user;
 	$output['err'] = FALSE;
-	$output['msg'] = "Got data: $data";
+	$output['msg'] = "Got data: $user";
 	
 	echo json_encode($output);
 ?>

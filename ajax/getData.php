5a9f16dfc3b64d101e5d5beb5760bac25a582f98
timmch <timmch@gmail.com>
2012-09-29 09:39:24 -0400

    D

--- ajax/getData.php
+++ ajax/getData.php
@@ -1,9 +1,12 @@
 <?php
-
 	$username = "timmch_root";
 	$password = "recentivize";
-	$hostname = "108.167.179.192";
-	$dbhandle = mysql_connect($hostname, $username, $password);
+	$hostname = "108.167.179.192"; 
+
+	$dbhandle = mysql_connect($hostname, $username, $password) 
+	  or die("Unable to connect to MySQL");
+	echo "Connected to MySQL<br>";
+
 	$selected = mysql_select_db("timmch_recentivize",$dbhandle);
 	$data = mysql_query("SELECT * FROM users");
 	$output['data'] = $data;

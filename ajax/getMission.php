de6b8b1a2af47f5f00520cb070c918d078791700
timmch <timmch@gmail.com>
2012-09-29 21:21:00 -0400

    D

--- ajax/getMission.php
+++ ajax/getMission.php
@@ -1,11 +1,9 @@
 <?php
+    session_start();
 	$username = "timmch_root";
 	$password = "recentivize";
 	$hostname = "108.167.179.192";
 	$id = "'".$_SESSION['user_id']."'";  
-	echo("***********88");
-	echo($id);
-	echo("*********************");
 	$dbhandle = mysql_connect($hostname, $username, $password) 
 	  or die("Unable to connect to MySQL");
 
